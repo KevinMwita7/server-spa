fe6e62742b1229012b3b84e0fdd8cac914e4655f
KevinMwita7 <kevinmwita7@gmail.com>
2023-10-08 11:01:29 +0300

    Fix up urls

--- server_spa/server_spa/urls.py
+++ server_spa/server_spa/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("demo/", include("demo_app.urls")),
+    path("", include("demo_app.urls")),
     path('admin/', admin.site.urls),
 ]
