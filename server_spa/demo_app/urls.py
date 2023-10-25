a3fb852b4d507bfce1d00c77c9be2bd518549019
KevinMwita7 <kevinmwita7@gmail.com>
2023-10-25 19:21:57 +0300

    Added form functionalities

--- server_spa/demo_app/urls.py
+++ server_spa/demo_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("pricing", views.pricing, name="demo_app_pricing"),
     path("faq", views.faq, name="demo_app_faq"),
     path("privacy", views.privacy, name="demo_app_privacy"),
-    path("about", views.about, name="demo_app_about")
+    path("about", views.about, name="demo_app_about"),
+    path("contact_us", views.contact_us, name="demo_app_contact_us"),
+    path("success", views.success, name="demo_app_success")
 ]
--- server_spa/demo_app/views.py
+++ server_spa/demo_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from .forms import ContactUsForm
 
 # Create your views here.
 def index(request):
@@ -15,6 +17,25 @@ def privacy(request):
 
 def about(request):
     return render(request, "demo_app/about.html", { "template": _base_template(request) })
+
+def contact_us(request):
+    if (request.method == "POST"):
+        form = ContactUsForm(request.POST)
+        if (form.is_valid()):
+            response = redirect("/success")
+            response.status_code = 303
+            return response
+    else:
+        form = ContactUsForm()
+
+    return render(request, "demo_app/contact_us.html", { "template": _base_template(request), "form": form })
+
+def success(request):
+    # This page can only be accessed on successful form submissions
+    if "X-Requested-With" not in request.headers or request.headers["X-Requested-With"] != "XMLHttpRequest": 
+        return HttpResponseRedirect("/")
+    return render(request, "demo_app/success.html", { "template": _base_template(request) })
+    
 
 # Python has no private access modifiers. To work around this, one adds an underscore (_) before the function
 # or variable to signal to other devs that they are meant to be private. Note that they can still be accessed
